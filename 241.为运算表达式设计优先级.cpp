adf39ed423e54dc5dd76f4d449cf9aa9edc576a0
WZY <13300240003@fudan.edu.cn>
2019-10-05 00:07:37 +0800

    321

---
+++ 241.为运算表达式设计优先级.cpp
@@ -0,0 +1,30 @@
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<regex>
+#include<sstream>
+#include<iostream>
+#include<stdio.h>
+#include<list>
+#include<cstdlib>
+using namespace std;
+//19/10/4
+class Solution {
+public:
+	string removeDuplicateLetters(string s)
+	{
+
+
+	}
+};
+int main()
+{
+	Solution x;
+	vector<int> res = x.diffWaysToCompute("2*3-4*5");
+	for (auto c : res)
+	{
+		cout << c << " ";
+	}
+	getchar();
+}
