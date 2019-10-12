1fc14a384850dccbe69d5daf4d3eb7b410061119
WZY <13300240003@fudan.edu.cn>
2019-10-13 01:48:03 +0800

    321更细

--- 321. 拼接最大数.cpp
+++ 321. 拼接最大数.cpp
@@ -8,55 +8,68 @@
 #include<cstdlib>
 #include<stack>
 using namespace std;
-//19/10/4
+//19/10/5凌晨
 class Solution {
 public:
-	string removeDuplicateLetters(string s)
+	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) 
 	{
-		stack<char> sta;
-		int last_exist[26] = { 0 };
-		for (int i = 0; i < s.size(); ++i)
+		int siz1 = nums1.size(), siz2 = nums2.size();
+		vector<int> rec1, rec2;	//两个数组分别记录两数组利用数字的序号
+		map<int, vector<int>> dp1, dp2;
+		cal_dp(dp1, nums1);
+		for (auto c : dp1)
 		{
-			last_exist[s[i] - 'a'] = i;
+			cout <<"长度:"<< c << endl;
+			for (auto x : dp1[c])
+			{
+				cout << x << " ";
+			}
+			cout << endl;
 		}
-		int visited[26] = { 0 };
-		for (int i=0;i<s.size();++i)
+		cal_dp(dp2, nums2);
+		return vector<int>();
+	}
+	void cal_dp(map<int,vector<int>>& dp, vector<int>& nums)
+	{
+		if (nums.size() == 0)
 		{
-			char c = s[i];
-			if (visited[c - 'a'])
+			return;
+		}
+		dp.clear();
+		int limit = max_element(nums.begin(), nums.end()) - nums.begin() + 1;
+		dp[0] = vector<int>();	//dp初始条件
+		dp[0].push_back(limit - 1);
+		for (int i = 1; i < nums.size(); ++i)
+		{
+			dp[i] = dp[i - 1];	//先复制之前的dp vector
+			if (limit == nums.size())	//到头了就回溯
 			{
-				continue;
-			}
-			if (sta.empty() or c>sta.top())
-			{
-				sta.push(c);
-			}
-			else
-			{
-				string cur_s = s.substr(i + 1, s.size());
-				while (!sta.empty() and sta.top() > c and last_exist[sta.top()-'a']>i)
+				int j = i - 1;
+				while (j>0 and dp[i][j] == dp[i][j - 1] + 1)
 				{
-					visited[sta.top() - 'a'] = 0;
-					sta.pop();
+					--j;
 				}
-				sta.push(c);
+				//从(dp[i][j-1],dp[i][j])找最大的数插进去
+				int le = 0, ri = dp[i][j];
+				if (j > 0)
+				{
+					le = dp[i][j - 1] + 1;
+				}
+				dp[i].insert(dp[i].begin() + j, max_element(nums.begin() + le, nums.begin() + ri) - nums.beign());
 			}
-			visited[c - 'a'] = 1;
+			else    //后面还有富余数字，直接添加一个最大的数字的索引到后面
+			{
+				dp[i].push_back(max_element(nums.begin() + dp[i].back() + 1, nums.end()) - nums.begin());
+			}
 		}
-		string res = "";
-		while (!sta.empty())
-		{
-			res += sta.top();
-			sta.pop();
-		}
-		reverse(res.begin(), res.end());
-		return res;
 	}
 };
 int main()
 {
 	Solution x;
-	string s = "cbacdcbc";
-	cout<<x.removeDuplicateLetters(s);
+	vector<int> nums1, nums2;
+	nums1 = { 3,4,6,5 };
+	nums2 = { 9,1,2,5,8,3 };
+	x.maxNumber(nums1, nums2);
 	getchar();
 }
